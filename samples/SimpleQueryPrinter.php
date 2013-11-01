c9d97c82fe2d685e7bbe33fc0eb278d2f0e11c69
Thibaud Fabre <thibaud@evaneos.com>
2013-11-01 20:00:26 +0100

    Update samples with group by statements

--- samples/SimpleQueryPrinter.php
+++ samples/SimpleQueryPrinter.php
@@ -1,16 +1,16 @@
 <?php
-
 use Skwal\TableReference;
 use Skwal\SelectQuery;
 use Skwal\Expression\LiteralExpression;
 use Skwal\Condition\ComparisonPredicate;
 use Skwal\CompOp;
+use Skwal\Query\Select;
 
 include __DIR__ . '/../Loader.php';
 
 $printer = new Skwal\Visitor\Printer\Query();
 $table = new TableReference('test');
-$query = new SelectQuery('childQuery');
+$query = new Select('childQuery');
 
 $query = $query->setTable($table)
     ->addColumn($table->getColumn('anyCol', 'aliasedCol'))
@@ -20,18 +20,22 @@ $query = $query->setTable($table)
     ->addColumn(new LiteralExpression(true, 'boolExpr'))
     ->addColumn(new LiteralExpression(null, 'nullExpr'));
 
+$query = $query->groupBy($query->getColumn(0))
+    ->groupBy($query->getColumn(1))
+    ->groupBy($query->getColumn(2));
+
 $condition = new ComparisonPredicate($query->deriveColumn(0), CompOp::Equals, new LiteralExpression(10));
 $condition = $condition->BOr($condition);
 $condition = $condition->BAnd($condition);
 
-$parentQuery = new SelectQuery('parent');
+$parentQuery = new Select('parent');
 $parentQuery = $parentQuery->setTable($query)
     ->addColumn($query->deriveColumn(0))
     ->addColumn($query->deriveColumn(2))
     ->setCondition($condition);
 
 echo '------------------------' . PHP_EOL;
-echo $printer->getQueryCommand($query) . PHP_EOL;
+echo $printer->printQuery($query) . PHP_EOL;
 echo '------------------------' . PHP_EOL;
-echo $printer->getQueryCommand($parentQuery) . PHP_EOL;
+echo $printer->printQuery($parentQuery) . PHP_EOL;
 echo '------------------------' . PHP_EOL;
